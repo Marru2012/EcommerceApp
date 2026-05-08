952842bf6d5f35dbff1020206e5abce592ad1728
JX4N <JX4N@Juan>
2026-05-08 01:02:57 -0500

    Creacion de contenido de ItemCarrito del paquete model

---
+++ src/main/java/com/mycompany/tiendaapp/model/ItemCarrito.java
@@ -0,0 +1,32 @@
+package com.mycompany.tiendaapp.model;
+
+import java.io.Serializable;
+
+public class ItemCarrito implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Producto producto;
+    private int      cantidad;
+
+    public ItemCarrito(Producto producto, int cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    public double getSubtotal() {
+        return producto.getPrecio() * cantidad;
+    }
+
+    public Producto getProducto()          { return producto; }
+    public void     setProducto(Producto p){ this.producto = p; }
+
+    public int  getCantidad()              { return cantidad; }
+    public void setCantidad(int c)         { this.cantidad = c; }
+
+    @Override
+    public String toString() {
+        return producto.getNombre() + " x" + cantidad
+                + " = $" + String.format("%.2f", getSubtotal());
+    }
+}
+
